a8246b539139c378f1f46cbeda1e2250905044eb
sarnakhmel <OlSa@MacBook-Pro-username.local>
2021-12-21 21:45:31 +0200

    init routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
+const HomeComponent = () => {
+  return <p>Home component</p>;
+};
+
+const ShopComponent = () => {
+  return <p>Shop component</p>;
+};
+
+const PhotosComponent = () => {
+  return <p>Photos component</p>;
+};
+
+const ContactUsComponent = () => {
+  return <p>Countact us component</p>;
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Routes app</h1>
+      <Switch>
+        <Route exact path="/">
+          <HomeComponent />
+        </Route>
+        <Route path="/shop">
+          <ShopComponent />
+        </Route>
+        <Route path="/photos">
+          <PhotosComponent />
+        </Route>
+        <Route path="/contact-us">
+          <ContactUsComponent />
+        </Route>
+      </Switch>
     </div>
   );
 }
